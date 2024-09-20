40ccb7544de27f01fb76a6e836f9c36353e0c8c4
Dan Vukota <danvukota@gmail.com>
2024-09-20 15:07:33 -0400

    Delete Create Equities table.sql

--- Create Equities table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [Financial_Securities].[Equities].[Equities](
-			[Ticker_ID] [int] IDENTITY (1, 1) NOT NULL,
-			[Ticker] [nchar](10) NOT NULL,
-			[Name] [nchar](50) NULL,
-                        [Sub_Industry_ID] [int] NOT NULL,
-        	CONSTRAINT PK_Equities PRIMARY KEY(Ticker_ID)),
-                CONSTRAINT FK_Equities_Sub_Industries FOREIGN KEY (Sub_Industry_ID)
-                REFERENCES [Financial_Securities].[Equities].Sub_Industries (Sub_Industry_ID));
