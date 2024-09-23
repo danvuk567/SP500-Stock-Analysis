5b4530f1a128c18c702b8a9940a86ce0782e4fa0
Dan Vukota <danvukota@gmail.com>
2024-09-23 10:53:23 -0400

    Delete Create-Database-Objects directory

--- Create-Database-Objects/Create-Industries-table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [Financial_Securities].[Equities].[Industries](
-	[Industry_ID] [int] NOT NULL,
-	[Name] [nchar](100) NULL,
-        [Industry_Group_ID] [int] NOT NULL,
-        CONSTRAINT PK_Industries PRIMARY KEY(Industry_ID));
