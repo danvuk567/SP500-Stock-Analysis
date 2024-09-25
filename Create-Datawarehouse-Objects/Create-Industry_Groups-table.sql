b5c61aa872362bffe1406f911561aeacd1416c03
Dan Vukota <danvukota@gmail.com>
2024-09-25 16:46:21 -0400

    Add files via upload

---
+++ Create-Datawarehouse-Objects/Create-Industry_Groups-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [Financial_Securities].[Equities].[Industry_Groups](
+	[Industry_Group_ID] [int] NOT NULL,
+	[Name] [nchar](100) NULL,
+        [Sector_ID] [int] NOT NULL,
+CONSTRAINT PK_Industry_Groups PRIMARY KEY(Industry_Group_ID));
