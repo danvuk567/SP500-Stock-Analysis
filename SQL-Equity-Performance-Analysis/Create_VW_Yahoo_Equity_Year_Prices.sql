0d28de9f19a3554f9546374c002a173a1174e923
Dan Vukota <danvukota@gmail.com>
2024-09-25 00:04:38 -0400

    Add files via upload

---
+++ SQL-Equity-Performance-Analysis/Create_VW_Yahoo_Equity_Year_Prices.sql
@@ -0,0 +1,28 @@
+CREATE VIEW [Equities].[VW_Yahoo_Equity_Year_Prices] AS
+WITH q1 AS
+(SELECT 
+	q2.Ticker_ID,
+   	TRIM(q3.Ticker) AS Ticker,
+	YEAR(q2.Date) AS "Year",
+	LAST_VALUE(q2.Date) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS Date,
+	FIRST_VALUE(q2.[Open]) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Open",
+	MAX(q2.[High]) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "High",
+ 	MIN(q2.[Low]) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Low",
+ 	LAST_VALUE(q2.[Close]) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Close",
+	LAST_VALUE(q2.Volume) OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "Volume",
+ 	ROW_NUMBER() OVER (PARTITION BY q2.Ticker_ID, YEAR(q2.Date) ORDER BY q2.Date DESC) AS Row_Num
+FROM [Financial_Securities].[Equities].[Yahoo_Equity_Prices] q2
+INNER JOIN [Financial_Securities].[Equities].[Equities] q3
+ON q2.Ticker_ID = q3.Ticker_ID)
+SELECT
+        q1.Ticker_ID,
+	q1.Ticker,
+	q1."Year",
+	q1.Date,
+	ROUND(q1."Open", 2) AS "Open",
+	ROUND(q1."High", 2) AS "High",
+	ROUND(q1."Low", 2) AS "Low",
+ 	ROUND(q1."Close", 2) AS "Close",
+	q1.Volume
+FROM q1
+WHERE q1.Row_Num = 1;
