e774c64cd9d0f6a28ca6acf5b59765132002712c
Dan Vukota <danvukota@gmail.com>
2024-09-25 16:48:01 -0400

    Update Create-Sub_Industries-table.sql

--- Create-Datawarehouse-Objects/Create-Sub_Industries-table.sql
+++ Create-Datawarehouse-Objects/Create-Sub_Industries-table.sql
@@ -2,4 +2,4 @@ CREATE TABLE [Financial_Securities].[Equities].[Sub_Industries](
 	[Sub_Industry_ID] [int] NOT NULL,
 	[Name] [nchar](100) NULL,
         [Industry_ID] [int] NOT NULL,
-        CONSTRAINT PK_Sub_Industries PRIMARY KEY(Sub_Industry_ID));
+CONSTRAINT PK_Sub_Industries PRIMARY KEY(Sub_Industry_ID));
