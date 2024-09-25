d58ef014afd477f607683b190d5390e8843018b0
Dan Vukota <danvukota@gmail.com>
2024-09-25 16:47:32 -0400

    Update Create-Industries-table.sql

--- Create-Datawarehouse-Objects/Create-Industries-table.sql
+++ Create-Datawarehouse-Objects/Create-Industries-table.sql
@@ -2,4 +2,4 @@ CREATE TABLE [Financial_Securities].[Equities].[Industries](
 	[Industry_ID] [int] NOT NULL,
 	[Name] [nchar](100) NULL,
         [Industry_Group_ID] [int] NOT NULL,
-        CONSTRAINT PK_Industries PRIMARY KEY(Industry_ID));
+CONSTRAINT PK_Industries PRIMARY KEY(Industry_ID));
