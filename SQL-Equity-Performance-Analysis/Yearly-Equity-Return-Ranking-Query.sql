5379843292b0142368c6e9d4d5e77e525cc93c90
Dan Vukota <danvukota@gmail.com>
2024-09-26 13:26:10 -0400

    Add files via upload

---
+++ SQL-Equity-Performance-Analysis/Yearly-Equity-Return-Ranking-Query.sql
@@ -0,0 +1,27 @@
+WITH q1 AS
+    (SELECT
+	Ticker_ID,
+	Ticker,
+	"Year",
+	"Date",
+	CASE
+	   WHEN COALESCE(LAG("Close", 1) OVER (PARTITION BY Ticker_ID ORDER BY "Date"), 0) = 0 THEN ROUND((("Close" / "Open") - 1.0) * 100, 2)
+	   ELSE ROUND((("Close" / LAG("Close", 1) OVER (PARTITION BY Ticker_ID ORDER BY "Date")) - 1.0) * 100, 2)
+	END AS "% Return"
+      FROM [Financial_Securities].[Equities].[VW_Yahoo_Equity_Year_Prices]),
+q2 AS
+    (SELECT 
+       q1.Ticker_ID,
+       q1.Ticker,
+       q1."Year",
+       q1."% Return",
+       RANK() OVER(PARTITION BY q1."Year" ORDER BY q1."% Return" DESC) AS "% Return Rank"
+     FROM q1)
+SELECT
+  q2."Year",
+  q2.Ticker,
+  q2."% Return",
+  q2."% Return Rank"
+FROM q2
+WHERE q2."% Return Rank" <= 5
+ORDER BY q2."Year", q2."% Return Rank";
